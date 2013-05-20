ed938b4dfdcec5b85b9addae2e5219764169ed25
adzen <101adzen@gmail.com>
2013-05-20 20:24:53 +0800

    SPOJ: Life, the Universe, and Everything (TEST)

---
+++ SPOJ/TEST.c
@@ -0,0 +1,11 @@
+/* Life, the Universe, and Everything */
+/* 9304268 0.00 1.6M */
+/* 腦殘的題目...三行秒殺XD */
+
+#include <stdio.h>
+
+int main(){
+   int in;
+   while( scanf("%d", &in) && in != 42 ) printf("%d\n", in);
+   return 0;
+}
