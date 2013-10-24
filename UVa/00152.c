4bdd7330af9043a1c69fea20dc9cfa4640054f33
adzen <101adzen@gmail.com>
2013-10-25 03:59:20 +0800

    Update 00152.c

--- UVa/00152.c
+++ UVa/00152.c
@@ -1,6 +1,10 @@
+/* 12555206 0.102 */
+/* 統計最近鄰居的距離 */
+
 #include <stdio.h>
 #include <math.h>
 
+/* 兩棵樹之間的距離^2 */
 int distance(int t1[], int t2[]){
    int sum = 0, i;
    for(i = 0; i < 3; ++i) sum += (t1[i]-t2[i]) * (t1[i]-t2[i]);
@@ -8,6 +12,7 @@ int distance(int t1[], int t2[]){
 }
 
 int main(){
+   /* 讀取樹的三維座標們 */
    int trees[5001][3], num = 0, i;
    while(1){
       int sum = 0;
@@ -19,6 +24,7 @@ int main(){
       else num++;
    }
    
+   /* 算出最近的鄰居距離 */
    int j, nearest[5000];
    char hasNearest[5000] = {0};
    for(i = 0; i < num; ++i){
@@ -40,9 +46,11 @@ int main(){
       }
    }}
    
+   /* 統計最近距離 < 10 的樹木們 */
    int ans[10] = {0};
    for(i = 0; i < num; ++i) if(nearest[i] < 100) ans[(int)sqrt(nearest[i])]++;
    
+   /* 印出答案 */
    for(i = 0; i < 10; ++i) printf("%4d", ans[i]);
    puts("");
 
