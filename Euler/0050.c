d29e4c0b603d65035e930ef2f78c4008b144b387
adzen <101adzen@gmail.com>
2013-11-28 18:00:55 +0800

    Update 0050.c

--- Euler/0050.c
+++ Euler/0050.c
@@ -1,3 +1,8 @@
+/* 一個質數可以用比它小、連續的若干個質數相加而成... */
+/* 小於 1 000 000 的質數中，哪個質數可以用最多連續的質數相加呢？ */
+
+/* 先篩法找出質數們，然後一一列舉連續的質數和 */
+
 #include <stdio.h>
 
 #define MAXTRY 1000000
@@ -6,6 +11,7 @@ char notprime[MAXTRY];
 int primes[100000], primen;
 
 int main(){
+   /* 還是篩法...老樣子... */
    int i = 2, j;
    while(i < MAXTRY){
       primes[primen++] = i;
@@ -23,8 +29,8 @@ int main(){
       int sum = 0, j = i;
       do{
          sum += primes[j];
-         if(sum >= MAXTRY) break;
-         if(!notprime[sum] && j-i+1 > maxNum){
+         if(sum >= MAXTRY) break;   /* 總和超過上限時，就不再列舉了 */
+         if(!notprime[sum] && j-i+1 > maxNum){   /* 總和是質數，且質數個數越多的會更新 */
             maxNum = j-i+1;
             ans = sum;
          }
@@ -32,7 +38,7 @@ int main(){
       }while(j < primen);
    }
 
-   printf("%d terms, sum = %d\n", maxNum, ans);
+   printf("%d terms, sum = %d\n", maxNum, ans);   /* 注意題目要的是那個質數，不是那一串的質數個數 */
    while(1);
    return 0;
 }
