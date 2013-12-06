b69ca733d0311f89adfee1528c1fb6aadb5b24f2
adzen <101adzen@gmail.com>
2013-12-07 01:58:49 +0800

    Update 0027.c

--- Euler/0027.c
+++ Euler/0027.c
@@ -1,3 +1,8 @@
+/* f(n) = n^2 + an + b, where |a| < 1000 and |b| < 1000
+   求可以讓 f(n) 從 n=0~K 皆會是質數的 (a,b)，且 K 是最大的... */
+   
+/* 先篩法建質數表...然後發現 n=0 時 f(n)=b，所以 b 只要列舉小於 1000 的質數即可 */
+
 #include <stdio.h>
 
 #define TOP 1000000
@@ -5,14 +10,16 @@
 char notprime[TOP];
 int primes[80000], primen;
 
+/* 檢查是不是質數 */
 int isprime(long long in){
-    if(in <= 1) return 0;
-    if(in < TOP) return 1-notprime[in];
-    printf("ERR");
+    if(in <= 1) return 0;   /* 包含負數 */
+    if(in < TOP) return 1-notprime[in];   /* 已經建表的直接用表格檢查 */
+    printf("ERROR!");   /* 剩下的例外好像因為質數表夠大，所以從來沒發生(?) */
     return 0;
 }
 
 int main(){
+    /* 老樣子的篩法 */
     int i = 2, j;
     while(i < TOP){
         primes[primen++] = i;
@@ -24,16 +31,15 @@ int main(){
         i++;
         while(i < TOP && notprime[i]) i++;
     }
-    /*printf("%d\n", primen);*/
     
     int a, bp, founda, foundb, maxn = 0;
-    for(bp = 0; primes[bp] < 1000; bp++){
+    for(bp = 0; primes[bp] < 1000; bp++){    /* b 列舉小於 1000 的質數即可 */
         int b = primes[bp];
-        for(a = -999; a < 1000; a++){
+        for(a = -999; a < 1000; a++){    /* a 則只好全都列舉了 */
             long long n;
             for(n = 1; isprime(n*n+a*n+b); n++);
             n--;
-            if(n > maxn){
+            if(n > maxn){   /* 找到更高的 K 就更新 */
                 founda = a;
                 foundb = b;
                 maxn = n;
