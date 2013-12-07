bade3693155ed5763a2f3a77cdefec10ec440fff
adzen <101adzen@gmail.com>
2013-12-07 21:59:12 +0800

    Update 0022.c

--- Euler/0022.c
+++ Euler/0022.c
@@ -1,3 +1,5 @@
+/* 字串字典排序+算字串分數的題目...算是秒殺題吧... */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,7 +14,7 @@ int cmp(const char *a, const char *b){
 int main(){
     freopen("input.txt","r",stdin);
     
-    char name[200] = {0};
+    /* 讀進所有名字並依字典排序 */
     while( scanf("\"%[A-Z]\",", &allname[num]) != EOF ) num++;
     qsort(allname, num, sizeof(char)*30, cmp);
     
@@ -20,8 +22,8 @@ int main(){
     long long ans = 0;
     for(i = 0; i < num; ++i){
         int j, score = 0;
-        for(j = 0; allname[i][j]; ++j) score += allname[i][j]-'A'+1;    
-        ans += (i+1) * score;
+        for(j = 0; allname[i][j]; ++j) score += allname[i][j]-'A'+1;  /* 算名字的分數 */    
+        ans += (i+1) * score;   /* 用分數和排名算答案 */
     }
     printf("%lld\n", ans);
 
