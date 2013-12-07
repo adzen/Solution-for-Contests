8c0645df42ae078a1f9106b83e496ff1fbb5a5c2
adzen <101adzen@gmail.com>
2013-12-07 22:34:20 +0800

    Update 0024.cpp

--- Euler/0024.cpp
+++ Euler/0024.cpp
@@ -1,3 +1,5 @@
+/* 數字頗小...所以直接用 STL 的 next_permutation() */
+
 #include <iostream>
 
 #include <algorithm>
@@ -7,7 +9,7 @@ using namespace std;
 int main(){
    int digits[10] = {0,1,2,3,4,5,6,7,8,9};
    
-   int count = 999999;
+   int count = 999999;   /* 還需要再轉 (一百萬 - 1) 次 */
    while( count && next_permutation(digits, digits+10) ) count--;
    
    for(int i = 0; i < 10; ++i) cout << digits[i];
