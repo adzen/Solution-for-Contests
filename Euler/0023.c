ea6f43de4c904c0cfeaf6d5a83e82270852fa635
adzen <101adzen@gmail.com>
2013-12-07 22:24:40 +0800

    Update 0023.c

--- Euler/0023.c
+++ Euler/0023.c
@@ -1,4 +1,7 @@
+/* 求所有「無法用兩個 abundant numbers 的和表示」的正整數總和... */
 
+/* 題目講了「大於 28123 的數字皆可用兩個 abundant numbers 相加」，
+   所以我們只要找 <= 28123 的數字即可 */
 
 #include <stdio.h>
 
@@ -6,6 +9,7 @@ int abundant[21200] = {12}, num = 1;
 char can[28124];
 
 int main(){
+    /* 尋找 13~28123 的 abundant number */
     int i;
     for(i = 13; i <= 28123; ++i){
         int sumOfDiv = 0, div;
@@ -14,6 +18,7 @@ int main(){
     }
     printf("num = %d\n", num);
     
+    /* 列舉兩個 abundant 相加的結果註記在 can[] 上 */
     int j;
     for(i = 0; i < num; ++i){
         for(j = i; j < num; ++j){
@@ -22,9 +27,10 @@ int main(){
         }
     }
     
+    /* 把 can[] 沒被註記的（無法用兩個 abundant 相加的數）加起來即可 */
     int ans = 0;
     for(i = 0; i <= 28123; ++i) if(can[i] == 0) ans += i;
     printf("ans = %d\n", ans);
-    while(1);
+
     return 0;
 }
