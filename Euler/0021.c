6d4b6bd89f6b803d09ef8c946d1c464f973b6bf7
adzen <101adzen@gmail.com>
2013-12-07 21:33:08 +0800

    Update 0021.c

--- Euler/0021.c
+++ Euler/0021.c
@@ -1,10 +1,15 @@
+/* 求小於一萬所有 amicable numbers 的總和... */
 
+/* 數字頗小，求因數和時直接試除即可... */
+
+/* 要注意 a ≠ b 這個條件 */
 
 #include <stdio.h>
 
-int dn[10000];
+int dn[10000];   /* 每個數字的 d(n) */
 
 int main(){
+    /* 先算 2~9999 的 d(n) */
     int i;
     for(i = 2; i < 10000; ++i){
         int sumOfDiv = 0, div;
@@ -12,8 +17,9 @@ int main(){
         dn[i] = sumOfDiv;
     }
     
+    /* 再把符合條件的加起來即可 */
     int ans = 0;
-    for(i = 2; i < 10000; ++i) if(dn[i] < 10000 && dn[i] != i && dn[dn[i]] == i) ans += i;
+    for(i = 2; i < 10000; ++i) if(dn[i] != i && dn[i] < 10000 && dn[dn[i]] == i) ans += i;
     printf("%d\n", ans);
     
     return 0;
