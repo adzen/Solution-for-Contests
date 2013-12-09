eae5a5c6e74d79e34c50c3f331197d316a382192
adzen <101adzen@gmail.com>
2013-12-10 00:34:06 +0800

    Update 0039.c

--- Euler/0039.c
+++ Euler/0039.c
@@ -1,17 +1,18 @@
+/* a+b+c = p, a^2+b^2 = c^2, 在 p <= 1000 的情況下，哪個 p 可以有最多的解？ */
 
-
-
+/* 先把 1000 以下的平方數算好備用，剩下就只是窮舉而已 */
 
 #include <stdio.h>
 
 int sq[1000];
 
 int main(){
+    /* 0^2 ~ 999^2 */
     int i;
     for(i = 0; i < 1000; i++) sq[i] = i*i;
     
     int p, maxp, maxnum = 0;
-    for(p = 12; p <= 1000; p++){
+    for(p = 12; p <= 1000; p++){   /* 最小的 p 是 3+4+5=12 */
         int solNum = 0, a;
         for(a = 1; a < p; a++){
             int bc = p-a, b;
