0e5b3c913aabc4568e4fe7e35b61322f7061e616
adzen <101adzen@gmail.com>
2013-12-10 00:16:50 +0800

    Update 0037.c

--- Euler/0037.c
+++ Euler/0037.c
@@ -1,7 +1,6 @@
+/* 「從左邊砍數字，和從右邊砍數字都會是質數」的質數，求這些質數的總和 */
 
-
-
-
+/* 篩法找出質數們，一一檢查其性質即可... */
 
 
 #include <stdio.h>
@@ -13,6 +12,7 @@ char notprime[TOP] = {1,1};
 int prime[80000], primen;
 
 int main(){
+    /* 老樣子的篩法 */
     int i = 2, j;
     while(i < TOP){
         prime[primen++] = i;
@@ -27,14 +27,16 @@ int main(){
     /*printf("primen = %d\n", primen);*/
     
     int ans = 0;
-    for(i = 4; i < primen; i++){   /* 略過 2,3,5,7 */
+    for(i = 4; i < primen; i++){   /* 要記得略過 2,3,5,7 */
         char dig[100];
         sprintf(dig, "%d", prime[i]);
         
+        /* 從左邊砍數字 */
         int j = 1;
         while(dig[j] && notprime[atoi(dig+j)] == 0) j++;
         if(dig[j] > 0) continue;
         
+        /* 從右邊砍數字 */
         j--;
         while(j > 0 && (dig[j]=0, notprime[atoi(dig)] == 0) ) j--;
         if(j > 0) continue;
