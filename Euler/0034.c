ebfa2c7e92051483239481f4007619b0c5045829
adzen <101adzen@gmail.com>
2013-12-10 13:09:20 +0800

    Update 0034.c

--- Euler/0034.c
+++ Euler/0034.c
@@ -1,26 +1,29 @@
+/*「數字的階乘總和 == 該數字大小」的數字總和 */
 
-
+/* 應該算簡單吧...就純粹列舉並檢查而已 */
 
 
 #include <stdio.h>
 
 int main(){
-    long long fact[10] = {1}, i;
+    /* 0! ~ 9! */
+    int fact[10] = {1}, i;
     for(i = 1; i < 10; ++i) fact[i] = fact[i-1] * i;
     
-    long long sum = 0;
+    int sum = 0;
     for(i = 3; i <= 1000000; ++i){
-        long long factDig = 0, j = i;
+        int factDig = 0, j = i;
         while(j > 0){
             factDig += fact[j%10];
             j /= 10;
         }
+        
         if(factDig == i){
-            printf("%lld\n", i);
+            printf("%d\n", i);
             sum += i;
         }
     }
-    printf("ans = %lld\n", sum);
+    printf("ans = %d\n", sum);
     
     return 0;
 }
